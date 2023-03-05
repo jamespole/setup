10346f438e40f652a0431f4bd8c836b039d61a2c
James Anderson-Pole <smart.ice9799@fastmail.com>
2023-03-05 23:59:20 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -31,6 +31,10 @@ sudo -u james git config --global user.name "James Anderson-Pole"
 sudo -u james git config --global user.email "smart.ice9799@fastmail.com"
 sudo -u james git config --global pull.rebase false
 
+# openssh-client config
+sudo -u james ssh-keygen -f /home/james/.ssh/id_ed25519 -t ed25519
+sudo -u james wget -O "/home/james/.ssh/authorized_keys" -- "https://github.com/jamespole.keys"
+
 # unattended-upgrades config
 ${install} 99local /etc/apt/apt.conf.d
 
