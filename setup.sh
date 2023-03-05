52f2784313ab887923a9adc241c0796dd139e1c9
James Anderson-Pole <smart.ice9799@fastmail.com>
2023-03-05 22:42:26 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,3 +16,5 @@ apt-get autoremove
 sudo -u james git config --global user.name "James Anderson-Pole"
 sudo -u james git config --global user.email "smart.ice9799@fastmail.com"
 sudo -u james git config --global pull.rebase false
+
+firewall-cmd --permanent --add-service=http
