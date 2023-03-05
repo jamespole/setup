58d79003fc5fdddd55e0845809758e12d5f0d5a8
James Anderson-Pole <smart.ice9799@fastmail.com>
2023-03-05 22:39:02 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ apt-get update
 apt-get upgrade
 
 apt-get install \
+    apache2 \
     firewalld \
     shellcheck \
     vim
 
 apt-get autoremove
+
+sudo -u james git config --global user.name "James Anderson-Pole"
+sudo -u james git config --global user.email "smart.ice9799@fastmail.com"
+sudo -u james git config --global pull.rebase false
