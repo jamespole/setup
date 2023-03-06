2916a4fad36d084904a392f34ea3caf70f65e464
James Anderson-Pole <smart.ice9799@fastmail.com>
2023-03-06 20:07:41 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,42 +10,45 @@ apt-get update
 apt-get upgrade
 
 apt-get install \
-    apache2 \
     borgbackup \
     composer \
     deborphan \
-    exim4-daemon-heavy \
-    firewalld \
+    screen \
     shellcheck \
-    unattended-upgrades \
     vim
-
-apt-get autoremove
 
 install='install --backup --compare --verbose --owner=root --group=root --mode=0644'
 
 # apache2 config
+apt-get install apache2
 a2enmod userdir
 
 # exim4 config
+apt-get install exim4-daemon-heavy
 ${install} mailname /etc
 ${install} update-exim4.conf.conf /etc/exim4
 
 # firewalld config
+apt-get install firewalld
 firewall-cmd --permanent --add-service=http
 
 # git config
+apt-get install git
 sudo -u james git config --global user.name "James Anderson-Pole"
 sudo -u james git config --global user.email "smart.ice9799@fastmail.com"
 sudo -u james git config --global pull.rebase false
 
 # openssh-client config
+apt-get install openssh-client
 key_path='/home/james/.ssh/id_ed25519'
 [ ! -f ${key_path} ] && sudo -u james ssh-keygen -f ${key_path} -t ed25519
 sudo -u james wget -O "/home/james/.ssh/authorized_keys" -- "https://github.com/jamespole.keys"
 
 # unattended-upgrades config
+apt-get install unattended-upgrades
 ${install} 99local /etc/apt/apt.conf.d
+
+apt-get autoremove
 
 # restart all services except firewalld
 systemctl restart apache2.service exim4.service
